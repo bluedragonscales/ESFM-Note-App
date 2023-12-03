5dbc7c8d6a2e74e26d3f675b3060e20f4f7da21c
KKarnoski <82429567+bluedragonscales@users.noreply.github.com>
2023-12-03 10:23:17 -0800

    Added more styling to the notes page

--- scripts/all-scripts.js
+++ scripts/all-scripts.js
@@ -7,6 +7,13 @@
 
 // NOTES PAGE
 
+// The "Add-Note" button will add a new note. A canvas box will pop-up and you can use your finger
+// or a mouse to draw/write a note.
+
+// The "Save" button inside the new note pop-up box will add that note to the list of notes.
+
+// The "X" button inside the canvas box will delete that note.
+
 
 
 // SHELF-LIST PAGE
