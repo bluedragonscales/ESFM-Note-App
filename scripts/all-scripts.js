d14cccbc224ce40dcaf8a76e5f2f7eb54f7ea56d
KKarnoski <82429567+bluedragonscales@users.noreply.github.com>
2023-12-03 09:13:35 -0800

    Added script page and fixed inventory button html

---
+++ scripts/all-scripts.js
@@ -0,0 +1,12 @@
+// INVENTORY PAGE
+
+// Use the plus and minus buttons to tick numbers in the quantity column.
+// Use the "Reset Button" button at the bottom to bring all numbers in the Quantity column to zero.
+
+
+
+// NOTES PAGE
+
+
+
+// SHELF-LIST PAGE
