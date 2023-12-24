661a2ab1e11ae0639199d7a98631b7e084f4a652
KKarnoski <82429567+bluedragonscales@users.noreply.github.com>
2023-12-24 11:30:31 -0800

    added id for plus/minus buttons

--- scripts/all-scripts.js
+++ scripts/all-scripts.js
@@ -1,5 +1,4 @@
 // INVENTORY PAGE
-
 // Use the plus and minus buttons to tick numbers in the quantity column.
 // Use the "Reset Button" button at the bottom to bring all numbers in the Quantity column to zero.
 
