1b46245858b2e85a3dc81261dd661daea66b15d0
KKarnoski <82429567+bluedragonscales@users.noreply.github.com>
2023-12-24 16:31:52 -0800

    Got rid of shelf list page and moved styles to folder

--- scripts/all-scripts.js
+++ scripts/all-scripts.js
@@ -1,6 +1,8 @@
 // INVENTORY PAGE
 // Use the plus and minus buttons to tick numbers in the quantity column.
 // Use the "Reset Button" button at the bottom to bring all numbers in the Quantity column to zero.
+let plusBtn = document.getElementById('plus');
+let minusBtn = document.getElementById('minus');
 
 
 
