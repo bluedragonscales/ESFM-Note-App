98cbc23911989b9a5e182557928052fa84a779da
KKarnoski <82429567+bluedragonscales@users.noreply.github.com>
2023-12-24 19:07:40 -0800

    Most of modal done for creating a new note

--- scripts/all-scripts.js
+++ scripts/all-scripts.js
@@ -11,9 +11,22 @@ let minusBtn = document.getElementById('minus');
 // or a mouse to draw/write a note.
 // The "Save" button inside the new note pop-up box will add that note to the list of notes.
 // The "X" button inside the canvas box will delete that note.
-let wholeModal = document.getElementById('simpleModal');
+let wholeModal = document.getElementById('wholeModal');
 let addNoteBtn = document.getElementById('addNoteBtn');
+let saveNoteBtn = document.getElementById('saveNoteBtn');
+let deleteNoteBtn = document.getElementById('deleteNoteBtn');
+
 
 function openModal() {
     wholeModal.style.display = 'block';
+    deleteNoteBtn.addEventListener('click', closeModal);
+    saveNoteBtn.addEventListener('click', saveNote);
+};
+
+function closeModal() {
+    wholeModal.style.display = 'none';
+};
+
+function saveNote() {
+
 }
